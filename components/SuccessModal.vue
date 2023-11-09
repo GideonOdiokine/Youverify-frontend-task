775955cfe7ccf4ba9d7f5b1133c107b851b3c454
GideonOdiokine <gidysmartodiokine@gmail.com>
2023-11-09 12:44:57 +0100

    successModal added

---
+++ components/SuccessModal.vue
@@ -0,0 +1,51 @@
+<template>
+  <modal @close="closeModal()">
+    <template v-slot:header>
+      <div class="flex justify-center mb-6">
+        <img src="../assets/tick.svg" alt="plus" />
+      </div>
+    </template>
+    <template v-slot:body>
+      <div class="text-center">
+        <h3 class="text-[#101828] text-[32px] font-semibold">Saved!</h3>
+        <p class="text-base text-[#626975] font-medium">
+          A new form has been added to your list. To see forms, click on the
+          button below.
+        </p>
+      </div>
+    </template>
+    <template v-slot:footer>
+      <button
+        @click="submitForm()"
+        class="py-3 rounded-[6px] mx-auto mt-6 flex justify-center items-center text-white text-base font-semibold px-6 bg-[#4D6CBB]"
+      >
+        close
+      </button>
+    </template>
+  </modal>
+</template>
+
+<script>
+export default {
+  props: {
+    close: Function,
+  },
+  data() {
+    return {
+      enableBtn: false,
+    };
+  },
+  methods: {
+    closeModal() {
+      if (typeof this.close === "function") {
+        this.close();
+      }
+    },
+    submitForm() {
+      //   console.log(this.title, this.description);
+      //   this.$emit("close");
+      navigateTo(`/`);
+    },
+  },
+};
+</script>
--- pages/details/[id]/index.vue
+++ pages/details/[id]/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mt-[80px] pt-6 md:px-6">
+  <div class="mt-[80px] p-5 pt-6 md:px-6">
     <div
       class="bg-white rounded-lg !h-[calc(100vh-120px)] overflow-y-scroll pb-6"
     >
@@ -14,9 +14,31 @@
             Templates
           </h3>
           <button
-            class="py-3 px-3 rounded-[6px] mb-[5px] w-full md:w-auto text-white text-base font-semibold md:px-6 bg-[#4D6CBB]"
+            @click="submitForm"
+            class="py-3 px-3 rounded-[6px] mb-[5px] flex w-full md:w-auto text-white text-base font-semibold md:px-6 bg-[#4D6CBB]"
           >
             Save
+            <svg
+              v-if="loading"
+              class="w-5 h-5 text-white animate-spin ml-3"
+              fill="none"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle
+                class="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+              ></circle>
+              <path
+                class="opacity-75"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                fill="currentColor"
+              ></path>
+            </svg>
           </button>
         </div>
       </div>
@@ -287,6 +309,9 @@
         </div>
       </div>
     </div>
+    <div v-if="showSuccessModal">
+      <SuccessModal />
+    </div>
   </div>
 </template>
 
@@ -297,6 +322,8 @@ export default {
       name: "",
       description: "",
       droppedItems: [],
+      loading: false,
+      showSuccessModal: false,
     };
   },
   methods: {
@@ -345,7 +372,14 @@ export default {
       // Insert the copied page right after the original
       this.droppedItems.splice(index + 1, 0, pageToCopy);
     },
-    printFormData() {
+    submitForm() {
+      this.loading = true;
+
+      setTimeout(() => {
+        // Set loading back to false
+        this.loading = false;
+        this.showSuccessModal = true;
+      }, 2000);
       // Collect and print the form data
       console.log("Name:", this.name);
       console.log("Description:", this.description);
