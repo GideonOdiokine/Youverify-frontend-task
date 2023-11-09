de415e465931550e2b5c0c129a86479760c35ac8
GideonOdiokine <gidysmartodiokine@gmail.com>
2023-11-09 03:11:05 +0100

    completed details page

--- pages/details/[id]/index.vue
+++ pages/details/[id]/index.vue
@@ -1,249 +1,275 @@
 <template>
   <div class="mt-[80px] pt-6 px-6">
     <div
-      class="bg-white rounded-lg !h-[calc(100vh-120px)] md:overflow-y-scroll lg:overflow-auto px-[32px] py-[36px]"
+      class="bg-white rounded-lg !h-[calc(100vh-120px)] md:overflow-y-scroll lg:overflow-auto"
     >
+      <!-- Top -->
+      <div class="border-b-[##ECEDEF] border-b mb-6">
+        <div class="text-[#667085] text-base pb-6 px-[32px] py-[36px]">
+          Form Templates /
+          <span class="font-medium">My Personalized Form</span>
+        </div>
+        <h3 class="text-[#101828] text-[32px] font-medium pb-6 px-[32px]">
+          Templates
+        </h3>
+      </div>
       <!-- Left -->
-      <div class="flex justify-between gap-6">
-        <div
-          :class="{
-            'border-[#4D6CBB] border-2 rounded-lg': droppedItems.length > 0,
-          }"
-          class="bg-[#F9FBFF] px-8 py-6 w-3/4 h-max"
-          @dragover.prevent
-          @drop="onDrop"
-        >
+      <div class="px-[32px]">
+        <div class="flex justify-between gap-6">
           <div
-            class="bg-white py-3 pb-6 mb-4 px-6 border border-[#DFE1E4] rounded-lg"
+            :class="{
+              'border-[#4D6CBB] border-2 rounded-lg': droppedItems.length > 0,
+            }"
+            class="bg-[#F9FBFF] px-8 py-6 w-3/4 h-max"
+            @dragover.prevent
+            @drop="onDrop"
           >
-            <label
-              for="name"
-              class="flex justify-between mx-0 !px-0 border-none text-[#101828] font-medium text-sm py-2"
+            <div
+              class="bg-white py-3 pb-6 mb-4 px-6 border border-[#DFE1E4] rounded-lg"
             >
-              <span class="px-[10px]">Subject:</span>
-              <img src="../../../assets/dot.svg" class="w-6" alt="" />
-            </label>
-            <input
-              type="text"
-              v-model="name"
-              id="name"
-              placeholder="Enter name"
-              class="border-[#DFE1E4] rounded-lg py-4 px-[10px] border w-full outline-none"
-            />
-          </div>
-          <div
-            class="bg-white py-3 pb-6 mb-4 px-6 border border-[#DFE1E4] rounded-lg"
-          >
-            <label
-              class="flex justify-between mx-0 !px-0 border-none text-[#101828] font-medium text-sm py-2"
-              for="description"
-            >
-              <span class="px-[10px]">Description:</span>
-              <img src="../../../assets/dot.svg" class="w-6" alt="" />
-            </label>
-            <input
-              type="text"
-              v-model="description"
-              id="description"
-              placeholder="Enter description"
-              class="border-[#DFE1E4] rounded-lg py-4 px-[10px] border w-full outline-none"
-            />
-          </div>
-          <div
-            v-for="(item, index) in droppedItems"
-            :key="index"
-            class="bg-white py-3 pb-6 mb-4 px-6 border border-[#DFE1E4] rounded-lg"
-          >
-            <div v-if="item.type === 'text'">
               <label
+                for="name"
                 class="flex justify-between mx-0 !px-0 border-none text-[#101828] font-medium text-sm py-2"
-                @blur="updateLabel($event, index)"
-                v-if="item.type === 'text'"
               >
-                <span class="px-[10px]">Text Input:</span>
+                <span class="px-[10px]">Subject:</span>
                 <img src="../../../assets/dot.svg" class="w-6" alt="" />
               </label>
               <input
-                v-if="item.type === 'text'"
                 type="text"
-                v-model="item.value"
-                id="text-input"
-                placeholder="Enter text"
+                v-model="name"
+                id="name"
+                placeholder="Enter name"
                 class="border-[#DFE1E4] rounded-lg py-4 px-[10px] border w-full outline-none"
               />
             </div>
-            <div v-else-if="item.type === 'checkbox'">
+            <div
+              class="bg-white py-3 pb-6 mb-4 px-6 border border-[#DFE1E4] rounded-lg"
+            >
               <label
-                for="checkbox-input"
                 class="flex justify-between mx-0 !px-0 border-none text-[#101828] font-medium text-sm py-2"
-                >Checkbox:</label
+                for="description"
               >
+                <span class="px-[10px]">Description:</span>
+                <img src="../../../assets/dot.svg" class="w-6" alt="" />
+              </label>
               <input
-                type="checkbox"
-                v-model="item.value"
-                id="checkbox-input"
-                class="bg-white py-3 pb-6 mb-4 px-6 border border-[#DFE1E4] rounded-lg"
+                type="text"
+                v-model="description"
+                id="description"
+                placeholder="Enter description"
+                class="border-[#DFE1E4] rounded-lg py-4 px-[10px] border w-full outline-none"
               />
             </div>
-
-            <div v-else-if="item.type === 'date'">
-              <label
-                class="flex justify-between mx-0 px-[10px] border-none text-[#101828] font-medium text-sm py-2"
-                v-if="item.type === 'date'"
-                >Date</label
-              >
-              <input
-                type="date"
-                v-model="item.value"
-                class="bg-white py-3 pb-6 mb-4 px-6 border border-[#DFE1E4] rounded-lg w-full"
-              />
-            </div>
-            <div v-else-if="item.type === 'page'">
-              <div
-                class="flex justify-between mx-0 !px-0 border-none text-[#101828] font-medium text-sm mb-4 py-2"
-                v-if="item.type === 'page'"
-              >
-                <div class="flex gap-4">
-                  <img
-                    src="../../../assets/saveIcon.svg"
-                    class="w-5 cursor-pointer"
-                    alt=""
-                  />
-                  <img
-                    src="../../../assets/copy.svg"
-                    class="w-5 cursor-pointer"
-                    alt=""
-                  />
-                  <img
-                    src="../../../assets/delete.svg"
-                    class="w-5 cursor-pointer"
-                    alt=""
-                    @click="removeItem(index)"
-                  />
-                </div>
-                <img
-                  src="../../../assets/dot.svg"
-                  class="w-5 cursor-pointer"
-                  alt=""
+            <div
+              v-for="(item, index) in droppedItems"
+              :key="index"
+              class="bg-white py-3 pb-6 mb-4 px-6 border border-[#DFE1E4] rounded-lg"
+            >
+              <div v-if="item.type === 'text'">
+                <label
+                  class="flex justify-between mx-0 !px-0 border-none text-[#101828] font-medium text-sm py-2"
+                  @blur="updateLabel($event, index)"
+                  v-if="item.type === 'text'"
+                >
+                  <span class="px-[10px]">Text Input:</span>
+                  <img src="../../../assets/dot.svg" class="w-6" alt="" />
+                </label>
+                <input
+                  v-if="item.type === 'text'"
+                  type="text"
+                  v-model="item.value"
+                  id="text-input"
+                  placeholder="Enter text"
+                  class="border-[#DFE1E4] rounded-lg py-4 px-[10px] border w-full outline-none"
                 />
               </div>
-              <div>
-                <div class="w-full justify-between items-center hidden md:flex">
-                  <div
-                    id="search"
-                    class="w-full rounded-[4px] border-b-[#DFE1E4] border-b mb-[24px] px-3 bg-gray-100 h-14 flex items-center"
-                  >
-                    <img
-                      src="../../../assets/pageIcon.svg"
-                      alt=""
-                      class="w-[24px] cursor-pointer mr-[12px] transform hover:translate-x-1 transition 200 ease-in"
-                    />
-                    <input
-                      type="search"
-                      v-model="item.newOption"
-                      class="w-11/12 bg-transparent text-sm outline-none"
-                      placeholder="Add Page description"
-                    />
-                  </div>
-                </div>
+              <div v-else-if="item.type === 'checkbox'">
+                <label
+                  for="checkbox-input"
+                  class="flex justify-between mx-0 !px-0 border-none text-[#101828] font-medium text-sm py-2"
+                  >Checkbox:</label
+                >
+                <input
+                  type="checkbox"
+                  v-model="item.value"
+                  id="checkbox-input"
+                  class="bg-white py-3 pb-6 mb-4 px-6 border border-[#DFE1E4] rounded-lg"
+                />
               </div>
-              <div
-                v-for="(option, optionIndex) in item.options"
-                :key="optionIndex"
-              >
-                <div class="flex justify-between pb-[28px]">
-                  <div class="space-x-3">
-                    <input
-                      type="radio"
-                      v-model="item.selectedOption"
-                      :value="option"
-                      :id="`option-${index}-${optionIndex}`"
-                    />
-                    <label
-                      class="text-sm text-[#101828] font-normal"
-                      :for="`option-${index}-${optionIndex}`"
-                      >{{ option }}</label
-                    >
-                  </div>
 
-                  <button @click="removeOption(index, optionIndex)">
+              <div v-else-if="item.type === 'date'">
+                <label
+                  class="flex justify-between mx-0 px-[10px] border-none text-[#101828] font-medium text-sm py-2"
+                  v-if="item.type === 'date'"
+                  >Date</label
+                >
+                <input
+                  type="date"
+                  v-model="item.value"
+                  class="bg-white py-3 pb-6 mb-4 px-6 border border-[#DFE1E4] rounded-lg w-full"
+                />
+              </div>
+              <div v-else-if="item.type === 'page'">
+                <div
+                  class="flex justify-between mx-0 !px-0 border-none text-[#101828] font-medium text-sm mb-4 py-2"
+                  v-if="item.type === 'page'"
+                >
+                  <div class="flex gap-4">
                     <img
-                      src="../../../assets/closePage.svg"
+                      src="../../../assets/saveIcon.svg"
                       class="w-5 cursor-pointer"
                       alt=""
                     />
-                  </button>
+                    <img
+                      src="../../../assets/copy.svg"
+                      class="w-5 cursor-pointer"
+                      alt=""
+                    />
+                    <img
+                      src="../../../assets/delete.svg"
+                      class="w-5 cursor-pointer"
+                      alt=""
+                      @click="removeItem(index)"
+                    />
+                  </div>
+                  <img
+                    src="../../../assets/dot.svg"
+                    class="w-6 cursor-pointer"
+                    alt=""
+                  />
                 </div>
+                <div>
+                  <div
+                    class="w-full justify-between items-center hidden md:flex"
+                  >
+                    <div
+                      id="search"
+                      class="w-full rounded-[4px] border-b-[#DFE1E4] border-b mb-[24px] px-3 bg-gray-100 h-14 flex items-center"
+                    >
+                      <img
+                        src="../../../assets/pageIcon.svg"
+                        alt=""
+                        class="w-[24px] cursor-pointer mr-[12px] transform hover:translate-x-1 transition 200 ease-in"
+                      />
+                      <input
+                        type="search"
+                        v-model="item.newOption"
+                        class="w-11/12 bg-transparent text-sm outline-none"
+                        placeholder="Add Page description"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div
+                  v-for="(option, optionIndex) in item.options"
+                  :key="optionIndex"
+                >
+                  <div class="flex justify-between pb-[28px]">
+                    <div class="space-x-3">
+                      <input
+                        type="radio"
+                        v-model="item.selectedOption"
+                        :value="option"
+                        :id="`option-${index}-${optionIndex}`"
+                      />
+                      <label
+                        class="text-sm text-[#101828] font-normal"
+                        :for="`option-${index}-${optionIndex}`"
+                        >{{ option }}</label
+                      >
+                    </div>
+
+                    <button @click="removeOption(index, optionIndex)">
+                      <img
+                        src="../../../assets/closePage.svg"
+                        class="w-5 cursor-pointer"
+                        alt=""
+                      />
+                    </button>
+                  </div>
+                </div>
+                <button
+                  @click="addOption(index)"
+                  class="text-[#4D6CBB] text-sm font-medium"
+                >
+                  <input type="radio" class="mr-3" />
+                  Add option
+                </button>
               </div>
-              <button
-                @click="addOption(index)"
-                class="text-[#4D6CBB] text-sm font-medium"
-              >
-                <input type="radio" class="mr-3" />
-                Add option
-              </button>
             </div>
+            <!-- <button @click="printFormData">Print Form Data</button> -->
           </div>
-          <!-- <button @click="printFormData">Print Form Data</button> -->
-        </div>
-        <!-- Right -->
-        <div class="w-2/4 border border-[#DFE1E4] rounded-md py-6 px-6">
-          <h3 class="text-[#101828] text-base font-medium pb-6">
-            Form Elements
-          </h3>
-          <h4 class="text-[#626975] text-sm font-medium pb-[18px]">INPUT</h4>
-          <div
-            class="flex items-center px-3 py-4 mb-4 border border-[#DFE1E4] rounded-md text-sm text-[#101828] font-medium"
-            @dragstart="onDragStart('text')"
-            draggable="true"
-          >
-            <img src="../../../assets/textField.svg" class="w-6 mr-2" alt="" />
-            Text Input
-          </div>
-          <div
-            class="flex items-center px-3 py-4 mb-4 border border-[#DFE1E4] text-sm text-[#101828] font-medium rounded-md"
-            @dragstart="onDragStart('date')"
-            draggable="true"
-          >
-            <img src="../../../assets/dateField.svg" class="w-6 mr-2" alt="" />
-            Date
-          </div>
-          <div
-            class="flex items-center px-3 py-4 mb-4 border border-[#DFE1E4] rounded-md text-sm text-[#101828] font-medium"
-            @dragstart="onDragStart('checkbox')"
-            draggable="true"
-          >
-            <img src="../../../assets/checkbox.svg" class="w-6 mr-2" alt="" />
-            Checkbox
-          </div>
-          <h4 class="text-[#626975] text-sm font-medium py-[18px]">LAYOUT</h4>
-          <div
-            class="flex items-center px-3 py-4 mb-4 border border-[#DFE1E4] text-sm text-[#101828] font-medium rounded-md"
-          >
-            <img src="../../../assets/group.svg" class="w-[27px] mr-2" alt="" />
-            Group
-          </div>
-          <div
-            class="flex items-center px-3 py-4 mb-4 border text-sm text-[#101828] border-[#DFE1E4] rounded-md font-medium"
-            @dragstart="onDragStart('page')"
-            draggable="true"
-          >
-            <img
-              src="../../../assets/pageIcon.svg"
-              class="w-[27px] mr-2"
-              alt=""
-            />
-            Page
-          </div>
-          <div
-            class="flex items-center px-3 py-4 mb-4 border border-[#DFE1E4] text-sm text-[#101828] rounded-md font-medium"
-          >
-            <img
-              src="../../../assets/listIcon.svg"
-              class="w-[27px] mr-2"
-              alt=""
-            />
-            List
+          <!-- Right -->
+          <div class="w-2/4 border border-[#DFE1E4] rounded-md py-6 px-6">
+            <h3 class="text-[#101828] text-base font-medium pb-6">
+              Form Elements
+            </h3>
+            <h4 class="text-[#626975] text-sm font-medium pb-[18px]">INPUT</h4>
+            <div
+              class="flex items-center px-3 py-4 mb-4 border border-[#DFE1E4] rounded-md text-sm text-[#101828] font-medium"
+              @dragstart="onDragStart('text')"
+              draggable="true"
+            >
+              <img
+                src="../../../assets/textField.svg"
+                class="w-6 mr-2"
+                alt=""
+              />
+              Text Input
+            </div>
+            <div
+              class="flex items-center px-3 py-4 mb-4 border border-[#DFE1E4] text-sm text-[#101828] font-medium rounded-md"
+              @dragstart="onDragStart('date')"
+              draggable="true"
+            >
+              <img
+                src="../../../assets/dateField.svg"
+                class="w-6 mr-2"
+                alt=""
+              />
+              Date
+            </div>
+            <div
+              class="flex items-center px-3 py-4 mb-4 border border-[#DFE1E4] rounded-md text-sm text-[#101828] font-medium"
+              @dragstart="onDragStart('checkbox')"
+              draggable="true"
+            >
+              <img src="../../../assets/checkbox.svg" class="w-6 mr-2" alt="" />
+              Checkbox
+            </div>
+            <h4 class="text-[#626975] text-sm font-medium py-[18px]">LAYOUT</h4>
+            <div
+              class="flex items-center px-3 py-4 mb-4 border border-[#DFE1E4] text-sm text-[#101828] font-medium rounded-md"
+            >
+              <img
+                src="../../../assets/group.svg"
+                class="w-[27px] mr-2"
+                alt=""
+              />
+              Group
+            </div>
+            <div
+              class="flex items-center px-3 py-4 mb-4 border text-sm text-[#101828] border-[#DFE1E4] rounded-md font-medium"
+              @dragstart="onDragStart('page')"
+              draggable="true"
+            >
+              <img
+                src="../../../assets/pageIcon.svg"
+                class="w-[27px] mr-2"
+                alt=""
+              />
+              Page
+            </div>
+            <div
+              class="flex items-center px-3 py-4 mb-4 border border-[#DFE1E4] text-sm text-[#101828] rounded-md font-medium"
+            >
+              <img
+                src="../../../assets/listIcon.svg"
+                class="w-[27px] mr-2"
+                alt=""
+              />
+              List
+            </div>
           </div>
         </div>
       </div>
